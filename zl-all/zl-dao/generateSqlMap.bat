271a178a418699ac9c1b193292a5f63ce4de05b1
is_zhoufeng <is_zhoufeng@163.com>
2015-06-02 18:32:50 +0800

    update

--- zl-all/zl-dao/generateSqlMap.bat
+++ zl-all/zl-dao/generateSqlMap.bat
@@ -1,4 +1,8 @@
 @echo off
+rd /Q /S "./generate_source/mapper"
+rd /Q /S "./generate_source/java"
+mkdir "./generate_source/mapper"
+mkdir "./generate_source/java"
 call mvn -Dmaven.test.skip=true mybatis-generator:generate 
 echo 文件生成完成
 pause
