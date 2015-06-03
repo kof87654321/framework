564d11cf3405bfc98405d5786d9de275f8df8ef9
is_zhoufeng <is_zhoufeng@163.com>
2015-06-03 16:42:16 +0800

    update

---
+++ zl-all/zl-web/src/main/java/com/zl/web/app/Consts.java
@@ -0,0 +1,34 @@
+package com.zl.web.app;
+
+/**
+ * 常量
+ * @author is_zhoufeng
+ */
+public interface Consts {
+	
+	/**
+	 * 资源文件中的key 
+	 */
+	public static class PropertiesKey{
+		
+		/**
+		 * 上传文件根目录
+		 */
+		public static final String APP_UPLOAD_DIR = "app.upload.dir";
+		/**
+		 * 项目地址
+		 */
+		public static final String APP_CONTEXT_PATH = "app.context.path";
+		/**
+		 * 静态资源服务器地址
+		 */
+		public static final String APP_STATICSERVER_PATH = "app.staticserver.path";
+		/**
+		 * 静态文件服务器地址
+		 */
+		public static final String APP_FILESERVER_PATH = "app.fileserver.path";
+		
+	}
+	
+
+}
--- zl-all/zl-web/src/main/java/com/zl/web/control/HelloWorldControl.java
+++ zl-all/zl-web/src/main/java/com/zl/web/control/HelloWorldControl.java
@@ -1,9 +1,11 @@
 package com.zl.web.control;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zl.client.user.UserService;
 import com.zl.common.util.bit.PropertiesConfigure;
 import com.zl.pojo.Lvuser;
+import com.zl.web.app.Consts;
 import com.zl.web.app.util.WebUtil;
 import com.zl.web.app.vo.AjaxResult;
 
@@ -36,27 +41,27 @@ public class HelloWorldControl {
 	private UserService userService ;
 	
 	/**
-	 * 普通http请求实例,浏览器输入http://127.0.0.1:8080/hello/index.htm进行访问
+	 * 普通http请求示例,浏览器输入http://127.0.0.1:8080/hello/index.htm进行访问
 	 * @param request
 	 * @param response
 	 * @param modelMap
 	 * @return
 	 */
 	@RequestMapping("index")
-	public String index(ServletRequest request, ServletResponse response , ModelMap modelMap) {
+	public String index(HttpServletRequest request, HttpServletResponse response , ModelMap modelMap) {
 		modelMap.put("userList", userService.selectAll()) ;     
 		log.info("index request...");
-		return "index";  
+		return "hello/index";  
 	}
 
 	/**
-	 * ajax请求实例（返回json内容）,浏览器输入http://127.0.0.1:8080/hello/json.htm进行访问
+	 * ajax请求示例（返回json内容）,浏览器输入http://127.0.0.1:8080/hello/json.htm进行访问
 	 * @param request
 	 * @param response
 	 * @param modelMap
 	 */
 	@RequestMapping("json") 
-	public void json(ServletRequest request, ServletResponse response , ModelMap modelMap) {
+	public void json(HttpServletRequest request, HttpServletResponse response) {
 		List<Lvuser> userList = userService.selectAll();
 		
 		/*
@@ -71,8 +76,46 @@ public class HelloWorldControl {
 				-1 /*这个参数是错误码，自定义*/ )
 				, response);
 		
-		
 		log.info("json request...");
 	}
+	
+	
+	/**
+	 * 去到上传文件页面
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("uploadView") 
+	public String uploadView(HttpServletRequest request, HttpServletResponse response){
+		return "hello/uploadView";
+	}
+	
+	/**
+	 * 上传文件示例
+	 * @param file
+	 * @param type
+	 * @param request
+	 * @param model
+	 */
+	@RequestMapping("upload") 
+	public void uplaod(@RequestParam("file")MultipartFile file, 
+			@RequestParam(value = "type" , required = false)String type, 
+			HttpServletRequest request, HttpServletResponse response ){
+		 log.info("type:{}",type);
+		 String filename = file.getOriginalFilename() ;
+		 log.info("filename:{}",filename);
+		 String uploadDir = propertiesConfigure.getProperties(Consts.PropertiesKey.APP_UPLOAD_DIR);
+		 log.info("uploadDir:{}",uploadDir);  
+		 File uplaodFile = new File(uploadDir,filename);
+		 try {
+			file.transferTo(uplaodFile);
+			WebUtil.ajaxOutput(AjaxResult.newSuccessResult(filename), response); 
+		} catch (IllegalStateException | IOException e) {
+			log.error(e.getMessage() , e);  
+			WebUtil.ajaxOutput(AjaxResult.newFailResult(null, "upload fail", -1), response);   
+		}
+	}
+	
 	
 }
