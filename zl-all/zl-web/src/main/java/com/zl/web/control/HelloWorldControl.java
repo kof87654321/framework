342b2f73c206843adcaaee04fead551093a13362
is_zhoufeng <is_zhoufeng@163.com>
2015-06-03 15:49:20 +0800

    update

--- zl-all/zl-web/src/main/java/com/zl/web/control/HelloWorldControl.java
+++ zl-all/zl-web/src/main/java/com/zl/web/control/HelloWorldControl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zl.client.user.UserService;
 import com.zl.common.util.bit.PropertiesConfigure;
