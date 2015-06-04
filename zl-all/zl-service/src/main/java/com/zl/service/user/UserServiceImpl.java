98997be431f2655d0c7ff8ceb5a6a2f194eec7b9
is_zhoufeng <is_zhoufeng@163.com>
2015-06-04 22:55:18 +0800

    update

--- zl-all/zl-service/src/main/java/com/zl/service/user/UserServiceImpl.java
+++ zl-all/zl-service/src/main/java/com/zl/service/user/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.zl.service.user;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,16 @@ import com.zl.pojo.LvuserExample;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
+	
+	private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
 
 	@Autowired
 	private LvuserMapperExt lvuserMapper ;
 	
 	@Override
 	public List<Lvuser> selectAll() {
+		log.info("com.zl.service.user.UserServiceImpl.selectAll()...");
 		return lvuserMapper.selectByExample(new LvuserExample());   
 	}
 
