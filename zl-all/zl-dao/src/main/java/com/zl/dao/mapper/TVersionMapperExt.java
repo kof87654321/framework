a0a81af75311fd57918e78ce4b18dfdc09f6f7b3
is_zhoufeng <is_zhoufeng@163.com>
2015-06-06 22:23:45 +0800

    t-version

---
+++ zl-all/zl-dao/src/main/java/com/zl/dao/mapper/TVersionMapperExt.java
@@ -0,0 +1,8 @@
+package com.zl.dao.mapper;
+
+/**
+ * 该类用于扩展com.zl.dao.mapper.TVersionMapper接口
+ * by is_zhoufeng@163.com 2015-06-06 22:21:31
+ */
+public interface TVersionMapperExt extends TVersionMapper {
+}
