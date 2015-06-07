80cfb66c73a180e4aa7c6dc076601045950c9bd9
bushyou <bushyou@sina.com>
2015-06-07 14:36:39 +0800

    修改db

---
+++ zl-all/zl-service/src/test/java/com/zl/service/UserFeedsServiceTest.java
@@ -0,0 +1,26 @@
+package com.zl.service;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zl.client.userFeeds.UserFeedsService;
+import com.zl.pojo.TUserFeeds;
+
+public class UserFeedsServiceTest extends BaseServiceTest{
+	private static final Logger log = LoggerFactory.getLogger(UserFeedsServiceTest.class);
+	
+	@Autowired
+	private UserFeedsService userFeedsService;
+	
+	@Test
+	public void testInsert(){
+		TUserFeeds tUserFeeds = new TUserFeeds();
+		tUserFeeds.setAttributes("123");
+		tUserFeeds.setLike(1);
+		tUserFeeds.setPageCount(1);
+		tUserFeeds.setUserId(1l);
+		userFeedsService.insertTUserFeeds(tUserFeeds );
+	}
+}
