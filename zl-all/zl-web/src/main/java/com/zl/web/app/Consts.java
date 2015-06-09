b41b536258ea29911aa6eb37b82150d4835b10a1
is_zhoufeng <is_zhoufeng@163.com>
2015-06-09 21:10:57 +0800

    upload

--- zl-all/zl-web/src/main/java/com/zl/web/app/Consts.java
+++ zl-all/zl-web/src/main/java/com/zl/web/app/Consts.java
@@ -35,5 +35,75 @@ public interface Consts {
 		
 	}
 	
+	/**
+	 * 上传文件相关常量
+	 */
+	public static interface Upload{
+		
+		/**
+		 * 文件所属业务
+		 */
+		public static enum BizType{
+			/**
+			 * 头像文件
+			 */
+			BIZ_TYPE_USERPHOTO((byte)1 , "/userphoto"),
+			/**
+			 * 动态文件
+			 */
+			BIZ_TYPE_FEDD((byte)2 , "/feedfile"),
+			/**
+			 * 聊天文件
+			 */
+			BIZ_TYPE_CHAT((byte)3 , "/chatfile") ;
+			/**
+			 * 业务类型
+			 */
+			private byte type;
+			
+			/**
+			 * 文件存放位置
+			 */
+			private String dir ; 
+			
+			private BizType(byte type , String dir) {
+				this.type = type ;
+				this.dir = dir ;
+			}
+			
+			public byte getType() {
+				return type;
+			}
+			
+			public String getDir() {
+				return dir;
+			}
+			
+			public static BizType getByType(byte type){
+				for (BizType bizType : values()) {
+					if(bizType.getType() == type){
+						return bizType ;
+					}
+				}
+				return null ;
+			}
+		}
+		
+		// --文件类型
+		/**
+		 * 图片文件
+		 */
+		public static final byte FILE_TYPE_IMG = 1;
+		/**
+		 * 语音文件
+		 */
+		public static final byte FILE_TYPE_VOICE = 2;
+		/**
+		 * 视频文件
+		 */
+		public static final byte FILE_TYPE_VIDEO = 3;
+		
+	}
+	
 
 }
---
+++ zl-all/zl-web/src/main/java/com/zl/web/control/FileController.java
@@ -0,0 +1,97 @@
+package com.zl.web.control;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.zl.client.file.FileService;
+import com.zl.common.util.bit.PropertiesConfigure;
+import com.zl.pojo.TFile;
+import com.zl.web.app.Consts;
+import com.zl.web.app.Consts.Upload.BizType;
+import com.zl.web.app.util.WebUtil;
+import com.zl.web.app.vo.AjaxResult;
+
+@Controller
+@RequestMapping("/file/")
+public class FileController {
+
+	private static final Logger log = LoggerFactory.getLogger(FileController.class);
+	
+	public static final String SEPARATOR = "/" ;
+
+	@Autowired
+	private FileService fileService ;
+
+	@Autowired
+	private PropertiesConfigure propertiesConfigure ;
+
+	/**
+	 * 上传文件
+	 * @param file
+	 * @param fileType [1:图片，2:语音 ,3:视频]
+	 * 
+	 * @param request
+	 * @param model
+	 */
+	@RequestMapping("upload") 
+	public void uplaod(@RequestParam("file")MultipartFile file, Byte fileType,Byte bizType
+			,HttpServletRequest request, HttpServletResponse response ){
+		String filename = file.getOriginalFilename() ;
+		String extension = FilenameUtils.getExtension(filename);
+		String baseDir = propertiesConfigure.getProperties(Consts.PropertiesKey.APP_UPLOAD_DIR);
+		BizType bizTypeEnum = Consts.Upload.BizType.getByType(bizType) ;
+		if(bizTypeEnum == null){
+			log.warn("不支持上传该业务类型的文件!"); 
+			WebUtil.ajaxOutput(AjaxResult.newFailResult(null, "不支持上传该业务类型的文件!", -1), response);  
+			return;
+		}
+		String saveDirPath = bizTypeEnum.getDir();
+		File saveDir = new File(baseDir,saveDirPath);
+		if(!saveDir.exists()){
+			saveDir.mkdirs();
+		}
+		String saveFileName = UUID.randomUUID().toString().replace("-", "") + "." + extension ; //保存的文件名随机生成 
+		File saveFile = new File(saveDir,saveFileName);  
+		try {
+			file.transferTo(saveFile);
+		} catch (IllegalStateException  | IOException e) {
+			log.error("保存文件失败" , e);  
+			WebUtil.ajaxOutput(AjaxResult.newExceptionResult(e, "保存文件失败", -2), response);   
+			return ;
+		}
+		
+		//文件相对路径
+		String saveFilePath = saveDirPath + SEPARATOR + saveFileName ;  
+		
+		TFile tfile = new TFile();
+		tfile.setBizType(bizType);
+		tfile.setFileType(fileType);
+		tfile.setFileName(saveFileName); 
+		tfile.setUrl(saveFilePath); 
+		
+		WebUtil.ajaxOutput(AjaxResult.newSuccessResult(tfile), response);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		String extension = FilenameUtils.getExtension("c:/aaa.txt"); 
+		System.out.println(extension);
+	}
+
+
+}
+
