b3a1bad9a27a4469eed14a440acf5629f5ec9467
kof87654321 <kof87654321@163.com>
2015-06-12 06:40:30 +0800

    kaifa

--- zl-all/zl-client/src/main/java/com/zl/client/userFeeds/UserFeedsService.java
+++ zl-all/zl-client/src/main/java/com/zl/client/userFeeds/UserFeedsService.java
@@ -7,8 +7,8 @@ import com.zl.pojo.TUserFeeds;
 
 public interface UserFeedsService {
 	public int insertTUserFeeds(TUserFeeds tUserFeeds);
-	public List<TUserFeeds> getUserFeedsList4TUserFeedsExample(long userId, int bigPageCount,long bigId,Page page);
-	public int getUserFeedsCount4TUserFeedsExample(long userId, int bigPageCount,long bigId);
+	public List<TUserFeeds> getUserFeedsList4TUserFeedsExample(long userId, int greaterThanOrEqualToPicCount,long bigId,Page page);
+	public int getUserFeedsCount4TUserFeedsExample(long userId, int greaterThanOrEqualToPicCount,long bigId);
 	public int addLike(Long id, int count);
 	
 }
