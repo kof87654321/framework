1bdfe35c590a52bbf6375e94f91104aadb94af76
zhangxj <sucanhh@163.com>
2015-06-11 17:03:17 +0800

    token 改进

--- zl-all/zl-common/src/main/java/com/zl/common/util/token/TokenUtils.java
+++ zl-all/zl-common/src/main/java/com/zl/common/util/token/TokenUtils.java
@@ -17,38 +17,39 @@ import org.springframework.util.StringUtils;
 import com.zl.common.util.encrypt.DesUtils;
 
 public class TokenUtils {
-	private static final Logger LOGGER = LoggerFactory.getLogger(TokenUtils.class);
+    private static final Logger LOGGER      = LoggerFactory.getLogger(TokenUtils.class);
 
-	private static final String ENCRYPT_KEY = "ABCD14CJO5F68DY69EQ5IWBYA3F2DESJ";
+    private static final String ENCRYPT_KEY = "ABCD14CJO5F68DY69EQ5IWBYA3F2DESJ";
 
-	private static String HENGXIAN = "_";
+    private static String       HENGXIAN    = "_";
 
-	public static String getToken(Long userId, String password, Date lastLoginTime) {
-		String message = MessageFormat.format("{0}" + HENGXIAN + "{1}" + HENGXIAN + "{2}",
-				String.valueOf(userId), password, String.valueOf(lastLoginTime.getTime()));
-		String token = null;
-		try {
-			token = DesUtils.encryptString(ENCRYPT_KEY, message);
-		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-				| IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
-			LOGGER.error("get token error!", e);
-		}
-		return token;
-	}
+    public static String getToken(Long userId, String password, Date lastLoginTime) {
+        String message = MessageFormat.format("{0}" + HENGXIAN + "{1}" + HENGXIAN + "{2}", String.valueOf(userId),
+            password, String.valueOf(lastLoginTime.getTime()));
+        String token = null;
+        try {
+            token = DesUtils.encryptString(ENCRYPT_KEY, message);
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+                | BadPaddingException | UnsupportedEncodingException e) {
+            LOGGER.error("get token error!", e);
+        }
+        return token;
+    }
 
-	public static boolean checkToken(Long userId, String password, String token) {
-		token = StringUtils.replace(token, " ", "+");
-		String tokenSource = null;
-		try {
-			tokenSource = DesUtils.decryptString(ENCRYPT_KEY, token);
-		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-				| IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
-			return false;
-		}
-		if (org.apache.commons.lang3.StringUtils.isBlank(tokenSource)) {
-			return false;
-		}
-		String[] tokens = tokenSource.split(HENGXIAN);
-		return (userId+"").equals(tokens[0]) && (password+"").equals(tokens[1]);
-	}
+    public static boolean checkToken(Long userId, String password, String token) {
+        token = StringUtils.replace(token, " ", "+");
+        String tokenSource = null;
+        try {
+            tokenSource = DesUtils.decryptString(ENCRYPT_KEY, token);
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+                | BadPaddingException | UnsupportedEncodingException e) {
+            return false;
+        }
+        if (org.apache.commons.lang3.StringUtils.isBlank(tokenSource)) {
+            return false;
+        }
+        String[] tokens = tokenSource.split(HENGXIAN);
+        //如果需要，解析出来的日期可以用来做token过期比对
+        return (userId + "").equals(tokens[0]) && (password + "").equals(tokens[1]);
+    }
 }
