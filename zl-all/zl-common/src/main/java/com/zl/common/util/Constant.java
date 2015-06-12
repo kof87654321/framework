bea30dd5722ef1d38869c7ae464e59098f74f144
kof87654321 <kof87654321@163.com>
2015-06-13 01:36:31 +0800

    kaifa

--- zl-all/zl-common/src/main/java/com/zl/common/util/Constant.java
+++ zl-all/zl-common/src/main/java/com/zl/common/util/Constant.java
@@ -2,9 +2,15 @@ package com.zl.common.util;
 
 public interface Constant {
 	public static interface STATUS {
-		public static int CHECKED = 1;//审核通过
-		public static int NOMARL = 0;//初始化
-		public static int NO_CHECK = -1;//审核不通过
-		public static int DELETE = -4;//删除
+		public static int CHECKED = 1;// 审核通过
+		public static int NOMARL = 0;// 初始化
+		public static int NO_CHECK = -1;// 审核不通过
+		public static int DELETE = -4;// 删除
+	}
+
+	public static interface ENV {
+		public static String TEST = "test";
+		public static String YUFA = "yufa";
+		public static String ONLINE = "online";
 	}
 }
---
+++ zl-all/zl-common/src/main/java/com/zl/common/util/ProjectEnv.java
@@ -0,0 +1,17 @@
+/**
+ * 环境变量
+ */
+package com.zl.common.util;
+
+public class ProjectEnv {
+	private String env;
+
+	public String getEnv() {
+		return env;
+	}
+
+	public void setEnv(String env) {
+		this.env = env;
+	}
+
+}
