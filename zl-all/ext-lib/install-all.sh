f264e66488bea156d2c5b99688097a00d348ec92
is_zhoufeng <is_zhoufeng@163.com>
2015-06-15 00:28:37 +0800

    umeng消息推送

--- zl-all/ext-lib/install-all.sh
+++ zl-all/ext-lib/install-all.sh
@@ -1,3 +1,4 @@
 mvn install:install-file  -Dfile=generator-0.0.1-RELEASE.jar  -DgroupId=mygenerator -DartifactId=generator -Dversion=0.0.1-RELEASE -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file  -Dfile=api-util-0.0.1-SNAPSHOT-classes.jar  -DgroupId=api -DartifactId=api-util -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -Dclassifier=classes -DgeneratePom=true
 mvn install:install-file  -Dfile=api-util-0.0.1-SNAPSHOT.war  -DgroupId=api -DartifactId=api-util -Dversion=0.0.1-SNAPSHOT -Dpackaging=war -DgeneratePom=true
+mvn install:install-file  -Dfile=push-0.0.1-SNAPSHOT.jar  -DgroupId=push -DartifactId=umeng-push -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
