8d4bd64e49e4666223874949f39a346704cd4bee
kof87654321 <kof87654321@163.com>
2015-06-14 13:01:49 +0800

    kaifa

--- zl-all/zl-dao/src/main/java/com/zl/dao/mapper/TCommentMapperExt.java
+++ zl-all/zl-dao/src/main/java/com/zl/dao/mapper/TCommentMapperExt.java
@@ -1,8 +1,14 @@
 package com.zl.dao.mapper;
 
+import java.util.List;
+
+import com.zl.pojo.TCommentExample;
+import com.zl.vo.TCommentCountVO;
+
 /**
  * 该类用于扩展com.zl.dao.mapper.TCommentMapper接口
  * by is_zhoufeng@163.com 2015-06-05 23:56:33
  */
 public interface TCommentMapperExt extends TCommentMapper {
+	 List<TCommentCountVO> countsByParentIdsAndExample(TCommentExample example);
 }
