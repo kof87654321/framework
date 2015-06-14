a9bc6a18951e18c6eaf88e5cc7eabc343fec131c
is_zhoufeng <is_zhoufeng@163.com>
2015-06-14 16:13:09 +0800

    新增邀请码表(T_Invite_Code) 评论表增加Status字段 用户表增加InviteCode字段

---
+++ zl-all/zl-dao/src/main/java/com/zl/dao/mapper/TInviteCodeMapperExt.java
@@ -0,0 +1,8 @@
+package com.zl.dao.mapper;
+
+/**
+ * 该类用于扩展com.zl.dao.mapper.TInviteCodeMapper接口
+ * by is_zhoufeng@163.com 2015-06-14 16:08:48
+ */
+public interface TInviteCodeMapperExt extends TInviteCodeMapper {
+}
