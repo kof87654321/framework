36dcf944a51218adaab438d31b48a2e180e5a673
kof87654321 <kof87654321@163.com>
2015-06-15 20:12:05 +0800

    kaifa

--- zl-all/zl-client/src/main/java/com/zl/client/user/TUserService.java
+++ zl-all/zl-client/src/main/java/com/zl/client/user/TUserService.java
@@ -7,24 +7,30 @@ import com.zl.pojo.TUser;
 import com.zl.pojo.TUserProfile;
 import com.zl.vo.TUserVO;
 
+/**
+ * 
+ * @see com.zl.service.user.impl.TUserServiceImpl
+ * @author youbush
+ *
+ */
 public interface TUserService {
 
-    public TUserVO getUserVOById(Long userId, boolean profile, boolean token);
+	public TUserVO getUserVOById(Long userId, boolean profile, boolean token);
 
-    public TUserVO updateUser(TUserVO tUserVO, boolean profile);
+	public TUserVO updateUser(TUserVO tUserVO, boolean profile);
 
-    public Long updateTUserProfile(TUserProfile tUserProfile, long userId, long id);
-    
-    public Long insertTUserProfile(List<TUserProfile> tUserProfileList, long userId);
+	public Long updateTUserProfile(TUserProfile tUserProfile, long userId, long id);
 
-    public List<TUserProfile> getTUserProfileList(long userId, long id,Page page);
+	public Long insertTUserProfile(List<TUserProfile> tUserProfileList, long userId);
 
-    public TUserVO insertUser(TUserVO tUserVO);
+	public List<TUserProfile> getTUserProfileList(long userId, long id, Page page);
 
-    public TUser getUserById(Long userId);
-    
-    public List<TUserVO> getListByAreaAndIndustry(int area,int industry,Page page);
-    
-    public int deleteTUserProfileByIdAndUserId(Long userId,long id);
+	public TUserVO insertUser(TUserVO tUserVO);
+
+	public TUser getUserById(Long userId);
+
+	public List<TUserVO> getListByAreaAndIndustry(int area, int industry, String key, Page page);
+
+	public int deleteTUserProfileByIdAndUserId(Long userId, long id);
 
 }
