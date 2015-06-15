3f1733d69cfc7deba851d2dfed83a3257f6f146f
kof87654321 <kof87654321@163.com>
2015-06-15 20:08:34 +0800

    kaifa

--- zl-all/zl-pojo/src/main/java/com/zl/vo/TUserVO.java
+++ zl-all/zl-pojo/src/main/java/com/zl/vo/TUserVO.java
@@ -7,34 +7,45 @@ import com.zl.pojo.TUser;
 import com.zl.pojo.TUserInfo;
 import com.zl.pojo.TUserProfile;
 
-public class TUserVO implements Serializable{
+/**
+ * 用户vo对象 用于结合TUser TUserInfo TUserProfile 前台显示使用
+ * 
+ * @author youbush
+ *
+ */
+public class TUserVO implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -2777905243908846977L;
-	
+
 	private TUser tUser;
 	private TUserInfo tUserInfo;
 	private List<TUserProfile> tUserProfileList;
+
 	public TUser gettUser() {
 		return tUser;
 	}
+
 	public void settUser(TUser tUser) {
 		this.tUser = tUser;
 	}
+
 	public TUserInfo gettUserInfo() {
 		return tUserInfo;
 	}
+
 	public void settUserInfo(TUserInfo tUserInfo) {
 		this.tUserInfo = tUserInfo;
 	}
+
 	public List<TUserProfile> gettUserProfileList() {
 		return tUserProfileList;
 	}
+
 	public void settUserProfileList(List<TUserProfile> tUserProfileList) {
 		this.tUserProfileList = tUserProfileList;
 	}
-	
 
 }
