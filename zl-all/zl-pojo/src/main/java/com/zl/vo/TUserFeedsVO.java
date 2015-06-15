51efa0ad98888a787097663203ced5e546ab5461
kof87654321 <kof87654321@163.com>
2015-06-15 20:41:59 +0800

    kaifa

--- zl-all/zl-pojo/src/main/java/com/zl/vo/TUserFeedsVO.java
+++ zl-all/zl-pojo/src/main/java/com/zl/vo/TUserFeedsVO.java
@@ -6,15 +6,20 @@ import java.util.List;
 import com.zl.pojo.TComment;
 import com.zl.pojo.TUserFeeds;
 
+/**
+ * 动态vo
+ * @author youbush
+ *
+ */
 public class TUserFeedsVO implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8125138207651561538L;
-	private TUserFeeds tUserFeeds;
-	private List<TCommentAndUserVO> tCommentAndUserVOList;
-	private TUserVO tUserVO;
+	private TUserFeeds tUserFeeds; //动态的内容
+	private List<TCommentAndUserVO> tCommentAndUserVOList; //动态里评论者的基本信息
+	private TUserVO tPostUserFeedsUserVO;//发布动态的用户信息
 
 	public TUserFeeds gettUserFeeds() {
 		return tUserFeeds;
@@ -24,14 +29,6 @@ public class TUserFeedsVO implements Serializable {
 		this.tUserFeeds = tUserFeeds;
 	}
 
-	public TUserVO gettUserVO() {
-		return tUserVO;
-	}
-
-	public void settUserVO(TUserVO tUserVO) {
-		this.tUserVO = tUserVO;
-	}
-
 	public List<TCommentAndUserVO> gettCommentAndUserVOList() {
 		return tCommentAndUserVOList;
 	}
@@ -39,6 +36,14 @@ public class TUserFeedsVO implements Serializable {
 	public void settCommentAndUserVOList(
 			List<TCommentAndUserVO> tCommentAndUserVOList) {
 		this.tCommentAndUserVOList = tCommentAndUserVOList;
+	}
+
+	public TUserVO gettPostUserFeedsUserVO() {
+		return tPostUserFeedsUserVO;
+	}
+
+	public void settPostUserFeedsUserVO(TUserVO tPostUserFeedsUserVO) {
+		this.tPostUserFeedsUserVO = tPostUserFeedsUserVO;
 	}
 
 }
