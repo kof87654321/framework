dd22b73fcec2eebc8fcab9844eeac90e6f13ae6e
kof87654321 <kof87654321@163.com>
2015-06-16 19:52:16 +0800

    kaifa

--- zl-all/zl-client/src/main/java/com/zl/client/userFeeds/UserFeedsService.java
+++ zl-all/zl-client/src/main/java/com/zl/client/userFeeds/UserFeedsService.java
@@ -5,6 +5,12 @@ import java.util.List;
 import com.zl.pojo.Page;
 import com.zl.pojo.TUserFeeds;
 
+/**
+ * 用户动态service
+ * 
+ * @author youbush
+ *
+ */
 public interface UserFeedsService {
 	public Long insertTUserFeeds(TUserFeeds tUserFeeds);
 	public List<TUserFeeds> getUserFeedsList4TUserFeedsExample(long userId, int greaterThanOrEqualToPicCount,long modifyTime,Page page);
